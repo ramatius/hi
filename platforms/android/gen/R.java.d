367b18026e2d983574af241d45b007cef0dbbe71
ramatius <ramatius@gmail.com>
2013-09-10 19:21:59 +0530

    First version
    Sample phonegap application - Hello World!

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+Y:\mobile_dev\phonegap\hi\platforms\android\gen\com\etamine\hi\R.java \
+ : Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable\icon.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-hdpi\icon.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-hdpi\ic_launcher.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-ldpi\icon.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-ldpi\ic_launcher.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-mdpi\icon.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-mdpi\ic_launcher.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-xhdpi\icon.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\layout\main.xml \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\values\strings.xml \
+Y:\mobile_dev\phonegap\hi\platforms\android\res\xml\config.xml \
+Y:\mobile_dev\phonegap\hi\platforms\android\bin\AndroidManifest.xml \
---
+++ platforms/android/gen/com/etamine/hi/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.etamine.hi;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
